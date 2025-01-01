6ff5b2d689207fceb9a142cd75dae5a95ffb04b4
Jackson Nace <jacksonnace964@gmail.com>
2025-01-01 17:41:26 -0600

    Skills flex issues

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -1,46 +1,52 @@
-import React from 'react'
-import '../components/Skills.css'
+import React from 'react';
+import '../components/Skills.css';
 // languages
-import cppImage from "../images/languages/C++.png"
-import JavaScriptImage from "../images/languages/Javascript.png"
-import LuaImage from "../images/languages/Lua.png"
-import pythonImage from "../images/languages/python.png"
-import RImage from "../images/languages/R.png"
-import SQLImage from "../images/languages/SQL.png"
+import cppImage from "../images/languages/C++.png";
+import JavaScriptImage from "../images/languages/Javascript.png";
+import LuaImage from "../images/languages/Lua.png";
+import pythonImage from "../images/languages/python.png";
+import RImage from "../images/languages/R.png";
+import SQLImage from "../images/languages/SQL.png";
 
 // Technologies
-import AWSImage from "../images/technologies/AWS.webp"
-import AzureImage from "../images/technologies/Azure.png"
-import CloudflareImage from "../images/technologies/Cloudflare.png"
-import GitImage from "../images/technologies/Git.png"
-import LinuxImage from "../images/technologies/Linux.png"
-import PostmanImage from "../images/technologies/Postman.png"
-import ReactImage from "../images/technologies/React.webp"
-import WindowsImage from "../images/technologies/Windows.png"
+import AWSImage from "../images/technologies/AWS.webp";
+import AzureImage from "../images/technologies/Azure.png";
+import CloudflareImage from "../images/technologies/Cloudflare.png";
+import GitImage from "../images/technologies/Git.png";
+import LinuxImage from "../images/technologies/Linux.png";
+import PostmanImage from "../images/technologies/Postman.png";
+import ReactImage from "../images/technologies/React.webp";
+import WindowsImage from "../images/technologies/Windows.png";
 
 function Skills() {
   return (
-    <div>
-      <div className="coding-languages">
-        <img src={cppImage} alt="C++" />
-        <img src={pythonImage} alt="Python" />
-        <img src={JavaScriptImage} alt="JavaScript" />
-        <img src={LuaImage} alt="Lua" />
-        <img src={RImage} alt="R" />
-        <img src={SQLImage} alt="SQL" />
+    <div className="skills-container">
+      <div className="skills-section">
+        <h2 className="skills-title">Coding Languages</h2>
+        <div className="coding-languages">
+          <img src={cppImage} alt="C++" />
+          <img src={pythonImage} alt="Python" />
+          <img src={JavaScriptImage} alt="JavaScript" />
+          <img src={LuaImage} alt="Lua" />
+          <img src={RImage} alt="R" />
+          <img src={SQLImage} alt="SQL" />
+        </div>
       </div>
-      <div className="technologies">
-        <img src={AWSImage} alt="AWS" />
-        <img src={AzureImage} alt="Azure" />  
-        <img src={CloudflareImage} alt="Cloudflare" />
-        <img src={GitImage} alt="Git" />
-        <img src={LinuxImage} alt="Linux" />
-        <img src={PostmanImage} alt="Postman" />
-        <img src={ReactImage} alt="React" />
-        <img src={WindowsImage} alt="Windows" />
+      <div className="skills-section">
+        <h2 className="skills-title">Technologies</h2>
+        <div className="technologies">
+          <img src={AWSImage} alt="AWS" />
+          <img src={AzureImage} alt="Azure" />
+          <img src={CloudflareImage} alt="Cloudflare" />
+          <img src={GitImage} alt="Git" />
+          <img src={LinuxImage} alt="Linux" />
+          <img src={PostmanImage} alt="Postman" />
+          <img src={ReactImage} alt="React" />
+          <img src={WindowsImage} alt="Windows" />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Skills
+export default Skills;
