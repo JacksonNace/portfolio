883b44bb5f6b219be6a96af12bdfd1b2af4c8d76
Jackson Nace <jacksonnace964@gmail.com>
2025-01-03 00:12:17 -0600

    UseState experimenting

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,6 +3,7 @@ import About from "./components/About";
 import Experience from "./components/Experience";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Skills />
       <About />
       <Experience />
+      <Projects />
     </div>
   );
 }
