28f2b2e707c6e3de3f00cc07fac4eb98dbd4cd4c
JacksonNace <jacksonnace964@gmail.com>
2025-01-03 15:03:59 -0600

    projects major update

--- portfolio/src/components/Experience.js
+++ portfolio/src/components/Experience.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Experience.css';
-// import UHLogo from '../images/UH.png';
-// import LavnerLogo from '../images/Lavner.webp';
-// import CoderKidsLogo from '../images/CoderKids.png';
+import UHLogo from '../images/UH.png';
+import LavnerLogo from '../images/Lavner.webp';
+import CoderKidsLogo from '../images/CoderKids.png';
 
 function Experience() {
   const experiences = [
@@ -15,7 +15,7 @@ function Experience() {
         "Installed new technology and learned foundational skills in re-imaging, Active Directory, diagnostics, and BIOS.",
         "Utilized Excel to manage and maintain an inventory of 110+ devices, including Computers, Simulation Robots, iPads, Cameras, and Printers ensuring optimal performance and timely updates."
       ],
-      //imageUrl: UHLogo
+      imageUrl: UHLogo
     },
     {
       company: "Lavner Education",
@@ -26,7 +26,7 @@ function Experience() {
         "Independently led classes of up to 25 students in topics like High School C++, Python, Intro to AI, and multiple variations of Robotics classes.",
         "Ended my internship teaching over 200+ students."
       ],
-      //imageUrl: LavnerLogo
+      imageUrl: LavnerLogo
     },
     {
       company: "Coder Kids",
@@ -36,7 +36,7 @@ function Experience() {
         "Conducted 150+ personalized lessons for students aged 8-16.",
         "Effectively planned, organized, and executed lessons in Python, Lua, and Roblox Studio.",
       ],
-      // imageUrl: CoderKidsLogo
+      imageUrl: CoderKidsLogo
     }
   ];
 
@@ -44,11 +44,11 @@ function Experience() {
     <div className="experience-container">
       {experiences.map((experience, index) => (
         <div className="experience-box" key={index}>
-          {/* <img
+          <img
             src={experience.imageUrl}
             alt={`${experience.company} logo`}
             className="experience-image"
-          /> */}
+          />
           <h3 className="experience-company">{experience.company}</h3>
           <p className="experience-date">{experience.date}</p>
           <h4 className="experience-jobTitle">{experience.jobTitle}</h4>
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,104 +1,103 @@
-
-// import React, { useState } from "react";
-// import "./Projects.css";
-
-// function Projects() {
-//   const [selectedProject, setSelectedProject] = useState(null);
-
-//   const projects = [
-//     {
-//       id: 1,
-//       title: "Project A",
-//       image: "https://via.placeholder.com/150",
-//       technologies: ["React", "CSS", "JavaScript"],
-//       description: "This is a brief description of Project A."
-//     },
-//     {
-//       id: 2,
-//       title: "Project B",
-//       image: "https://via.placeholder.com/150",
-//       technologies: ["Next.js", "Supabase", "Tailwind"],
-//       description: "This is a brief description of Project B."
-//     },
-//     {
-//       id: 3,
-//       title: "Project C",
-//       image: "https://via.placeholder.com/150",
-//       technologies: ["Python", "Flask", "SQLAlchemy"],
-//       description: "This is a brief description of Project C."
-//     }
-//   ];
-
-//   const toggleDescription = (id) => {
-//     setSelectedProject((prev) => (prev === id ? null : id));
-
-//   };
-
-//   return (
-//     <div className="projects-container">
-//       {projects.map((project) => (
-//         <div key={project.id} className="project-card">
-//           <img src={project.image} alt={project.title} className="project-image" />
-//           <h3 className="project-title">{project.title}</h3>
-//           <ul className="project-technologies">
-//             {project.technologies.map((tech, index) => (
-//               <li key={index}>{tech}</li>
-//             ))}
-//           </ul>
-//           <button
-//             className="toggle-description"
-//             onClick={() => toggleDescription(project.id)}
-//           >
-//             {selectedProject === project.id ? "Hide Details" : "Show Details"}
-//           </button>
-//           {selectedProject === project.id && (
-//             <p className="project-description">{project.description}</p>
-//           )}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default Projects;
-
-import React, { useState } from "react";
+import React from "react";
 import "./Projects.css";
+import GithubLogo from "../images/Github.svg"
 
-function Projects(){
-  const[selectedProject, setselectedProject] = useState(null) // starts with null because nothing is selected yet
+function Projects() {
 
   const projects = [
     {
       id: 1,
-      title: "Project A",
-      imageUrl: "www.google.com",
-      technologies: ["React, CSS, HTML"],
-      description: "Brief description",
+      title: "Portfolio Website",
+      imageUrl: "www.google.com", 
+      technologies: ["React, JavaScript, Azure Function, CosmosDB"],
+      description: [
+        "yeppers",
+        " cloud resume challenge.",
+      ],
+      githubLink: "https://github.com/JacksonNace/portfolio", // i have a conditional check in the JSX below that checks if the GithubLink is valid
     },
     {
       id: 2,
-      title: "Project A",
-      imageUrl: "www.google.com",
-      technologies: ["React, CSS, HTML"],
-      description: "Brief description",
+      title: "CodeRED Astra 2024",
+      imageUrl: "www.google.com", 
+      technologies: ["React", "JavaScript"],
+      description: [
+        "Built 8 frontend components in React.",
+        "Prepared for CodeRED Astra 2024.",
+        "Used libraries like Ant Design.",
+      ],
+      githubLink: null,
     },
     {
       id: 3,
-      title: "Project A",
-      imageUrl: "www.google.com",
-      technologies: ["React, CSS, HTML"],
-      description: "Brief description",
-    }
+      title: "Bleach RPG",
+      imageUrl: "www.google.com", 
+      technologies: ["C++"], 
+      description: [
+        "Created a turn-based battle system inspired by Bleach, with mechanics using inheritance and polymorphism.",
+        "Developed a save/load system with file handling to ensure seamless continuation of gameplay sessions",
+      ], 
+      githubLink: "https://github.com/JacksonNace/BleachRPG",
+    },
+    {
+      id: 4,
+      title: "Mockey Interview - HackTX 2024",
+      imageUrl: "www.google.com", 
+      technologies: ["PostgreSQL", "NodeJS", "NextJS"],
+      description: [
+        "Built a mock interview platform for HackTX 2024.",
+        "Engineered a mock interview game with Next.js and PostgreSQL, securing player data with password hashing and JWT - Hosted using Neon.",
+        "Integrated Claude AI as a virtual interviewer, offering real-time prompts and feedback on data structure tasks.",
+      ], 
+      githubLink: "https://github.com/JacksonNace/MockeyInterview-HackTX",
+    },
+    {
+      id: 5,
+      title: "MarketMentor - HackRice 2024",
+      imageUrl: "www.google.com", 
+      technologies: ["React", "CSS", "HTML"],
+      description: [
+        "Created a website to trade stocks with fake cash, connecting to the Llama API and displaying graphs.",
+        "Developed and integrated the Google Gemini API, enabling seamless data retrieval and enhancing app functionality.",
+        "Leveraged Postman to troubleshoot 7 API calls, ensuring smooth interaction between the frontend and backend",
+      ], 
+      githubLink: "https://github.com/JacksonNace/MarketMentor-HackRice",
+    },
   ];
 
-  
-
   return (
-    <div>
+    <div className="projects-container">
+      {projects.map((project) => (
+        <div key={project.id} className="project-card">
+          <img
+            src={project.imageUrl} 
+            alt={project.title} 
+            className="project-image" 
+          />
+          <h3 className="project-title">{project.title}</h3>
+          <ul className="project-technologies">
+            {project.technologies.map((tech, index) => (
+              <li key={index}>{tech}</li> 
+            ))}
+          </ul>
+          <ul className="project-description">
+            {project.description.map((point, index) => (
+              <li key={index}>{point}</li> 
+            ))}
+          </ul>
 
+          <div className="github-logo-container"> {/*conditional github logo*/} 
+            {project.githubLink && (
+              <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+                <img src={GithubLogo} alt="GitHub Logo" className="github-logo" />
+              </a>
+            )}
+
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default Projects;
