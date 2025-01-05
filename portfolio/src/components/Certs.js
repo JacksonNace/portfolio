f2b7fa190e8d20f0e67164037bf7f15db73b5b70
Jackson Nace <jacksonnace964@gmail.com>
2025-01-04 19:41:37 -0600

    add files/change styling for certs.js

---
+++ portfolio/src/components/Certs.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import './Certs.css';
+import AzureImg from "../images/azure.png";
+import bleach from "../images/ichigopfp.jpg";
+
+function Certs() {
+  const certificates = [
+    {
+      id: 1,
+      src: AzureImg,
+      title: 'Microsoft Azure Fundamentals',
+      description: 'This is the Microsoft Azure Fundamentals certification.',
+    },
+    {
+      id: 2,
+      src: bleach,
+      title: 'Certificate 2',
+      description: 'This is another certificate description.',
+    },
+  ];
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleSlideChange = (e) => {
+    const newIndex = Array.from(e.target.parentNode.children).indexOf(e.target);
+    setActiveIndex(newIndex);
+  };
+
+  return (
+    <div className="Cert-container">
+      <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          {certificates.map((cert, index) => (
+            <div
+              key={cert.id}
+              className={`carousel-item ${index === activeIndex ? 'active' : ''}`}
+              onClick={() => setActiveIndex(index)}
+            >
+              <img src={cert.src} className="d-block w-100" alt={cert.title} />
+            </div>
+          ))}
+        </div>
+
+        <a
+          className="carousel-control-prev"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-bs-slide="prev"
+          onClick={() => setActiveIndex((prev) => (prev - 1 + certificates.length) % certificates.length)}
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </a>
+        <a
+          className="carousel-control-next"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-bs-slide="next"
+          onClick={() => setActiveIndex((prev) => (prev + 1) % certificates.length)}
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </a>
+      </div>
+
+      <div className="carousel-caption-below">
+        <h5>{certificates[activeIndex].title}</h5>
+        <p>{certificates[activeIndex].description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Certs;
