a0fdf83fd4c58797a86aad8447b3c9de4bc53931
Jackson Nace <jacksonnace964@gmail.com>
2025-01-04 19:20:57 -0600

    npm install bootstrap/setup cert carousel

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
+
 import About from "./components/About";
+import Certs from "./components/Certs";
+import Education from "./components/Education";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
 import Experience from "./components/Experience";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
@@ -14,6 +21,8 @@ function App() {
       <Skills />
       <About />
       <Experience />
+      <Education />
+      <Certs />
       <Projects />
     </div>
   );
