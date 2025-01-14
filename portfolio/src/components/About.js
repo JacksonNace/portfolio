35b551dfd3cbde86eb83d237f20406436ac10ef5
Jackson Nace <jacksonnace964@gmail.com>
2025-01-13 22:12:07 -0600

    hero style

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,18 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import './About.css';
 
 function About() {
-  const [visitorCount, setVisitorCount] = useState(0);
-
-  useEffect(() => {
-    fetch("https://your-backend-api-url.com/visitor-counter")
-      .then(response => response.json())
-      .then(data => {
-        setVisitorCount(data.count);
-      })
-      .catch(error => console.error("Error fetching visitor count:", error));
-  }, []);
-
   return (
     <div className="about-container">
       <div className="about-me">
@@ -22,10 +11,6 @@ function About() {
             Hello! I'm Jackson, a passionate student and upcoming IT/Software professional. 
             I am always looking for new opportunities to learn and grow as both a student and a professional in every tech career field.
           </p>
-          <div className="visitor-counter">
-            <h2>Visitor Counter</h2>
-            <p>Number of visitors: {visitorCount}</p>
-          </div>
         </div>
         <div className="about-image">
           <img src="images/ichigopfp.jpg" alt="profile picture" className="profile-image" />
--- portfolio/src/components/Hero.js
+++ portfolio/src/components/Hero.js
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Hero.css'; 
 
 import Skills from "./Skills";
 import "./Skills.css";
 
-const Hero = () => {
+const Hero = () => {  
+  
   return (
     <section className="hero">
-      <div className="hero-content">
-        <div className="hero-text">
-          <h1>Hey, I'm Jackson!</h1>
-          <p>Studying computer science with a minor in math @ the University of Houston.</p>
-          <h3>Working @ University of Houston</h3>
-          <h3>Developing solutions 4 hackathons @ CodeRED</h3>
-        </div>
-        <div className="hero-image">
-          <img src="images/ichigopfp.jpg" alt="profile picture" />
-        </div>
-        <div className="hero-socials">
-          <a href="https://github.com/JacksonNace" target="_blank" rel="noopener noreferrer">
-            <img src="images/socials/Github.svg" alt="GitHub" />
-          </a>
-          <a href="https://www.linkedin.com/in/jacksonnace/" target="_blank" rel="noopener noreferrer">
-            <img src="images/socials/LinkedIn.png" alt="LinkedIn" />
-          </a>
-        </div>
+      <div className="hero-container">
+        <div className='con'></div>
+          <div className="hero-text">
+            <h1>Hey, I'm Jackson!</h1>
+            <p>Studying computer science with a minor in math @ the University of Houston.</p>
+            <h3>Working @ University of Houston</h3>
+            <h3>Developing solutions 4 hackathons @ CodeRED</h3>
+          </div>
+          <div className='hero-image-container'>
+            <div className="hero-image">
+              <img src="images/ichigopfp.jpg" alt="profile picture" />
+            </div>
+            <div className="hero-socials">
+              <a href="https://github.com/JacksonNace" target="_blank" rel="noopener noreferrer">
+                <img src="images/socials/Github.svg" alt="GitHub" />
+              </a>
+              <a href="https://www.linkedin.com/in/jacksonnace/" target="_blank" rel="noopener noreferrer">
+                <img src="images/socials/LinkedIn.png" alt="LinkedIn" />
+              </a>
+            </div>
+          </div>
       </div>
+        <div className="visitor-counter">
+          <h2>Visitor Counter</h2>
+          {/* <p>Number of visitors: {visitorCount}</p> */}
+        </div>
       <div className="skills-container">
         <Skills />
       </div>
