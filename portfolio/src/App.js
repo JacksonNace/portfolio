c9a6352de394419cacd982b0b0a8efc846e593f1
Jackson Nace <jacksonnace964@gmail.com>
2025-01-14 23:46:14 -0600

    GSAP is OP bro

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import { useLayoutEffect, useRef } from "react"
+import gsap from "gsap"
 
 import About from "./components/About";
 import Certs from "./components/Certs";
 import Education from "./components/Education";
-
 
 import Experience from "./components/Experience";
 import Footer from "./components/Footer"
@@ -12,15 +13,53 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 
+
+//.from() - opacity from->current 
+//.to() - opacity current->to
+
+
 function App() {
+
+  const comp = useRef(null)
+
+  useLayoutEffect(() => { //loads after the dom is done loading, would be great for codered hackathon components if we have a lot of random 3d 
+    //BRO PLEASEEEE WATYCH A TUTORIAL ON THIS I NEED MORE
+    let ctx = gsap.context(() => {
+      const t1 = gsap.timeline() // gsap timeline simplifies sequences for animations
+      t1.from("#intro-slider", {
+         xPercent: "-100", 
+         duration: 1.3,
+         delay:0.3,
+      }).from(["#title-1", "#title-2", "#title-3"], {
+        opacity: 0,
+        y: "+=30",
+        stagger:0.5,
+      }).to(["#title-1", "#title-2", "#title-3"], {
+        opacity: 0,
+        y: "-=30",
+        delay:0.3,
+        stagger: 0.5,
+      }).to("#intro-slider", {
+        xPercent: "-100",
+        duration: 1.3,
+      }).from("#app", {
+        opacity:0,
+        duration: 0.5,
+      })
+    }, comp)
+    return () => ctx.revert() // prevents memory leak with the library
+  }, []) 
   return (
-    <div className="app-container">
-      <div className="Intro">
-        <div>Software Engineer</div>
-        <div>Student</div>
-        <div>Software Engineer</div>
+    <div className="app-container" ref={comp}>
+      <div 
+        id="intro-slider"
+        className="Intro"
+      >
+        <div className="role" id="title-1">Software Engineer</div>
+        <div className="role" id="title-2">Software Engineer</div>
+        <div className="role" id="title-3">Software Engineer</div>
       </div>
-      <div className="App">
+      <div className="App" id="app">
         {/* <Navbar /> */}
         <Hero />
         <About />
