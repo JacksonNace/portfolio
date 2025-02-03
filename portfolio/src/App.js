f07b0c67a5c43a3b9488f53db982ca66fea760f4
Jackson Nace <jacksonnace964@gmail.com>
2025-02-02 20:14:32 -0600

    base layout

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,90 +1,8 @@
-// import React from "react";
-// import "./App.css";
-// import { useLayoutEffect, useRef } from "react"
-// import gsap from "gsap"
-// import Hero from "./components/Hero";
-// import Experience from "./components/Experience";
-// import Certs from "./components/Certs";
-// import Education from "./components/Education";
-
-// //import Experience from "./components/Experience";
-// import Footer from "./components/Footer"
-// import Navbar from "./components/Navbar";
-// import Projects from "./components/Projects";
-
-// //SHELVED
-// //import ScrollSection from "./components/ScrollSection"
-// // import About from "./components/About";
-
-// //.from() - opacity from->current 
-// //.to() - opacity current->to
-
-
-// function App() {
-
-//   const comp = useRef(null)
-//   //intro
-//   useLayoutEffect(() => { //loads after the dom is done loading, would be great for codered hackathon components if we have a lot of random 3d 
-//     //BRO PLEASEEEE WATYCH A TUTORIAL ON THIS I NEED MORE
-//     let ctx = gsap.context(() => {
-//       const t1 = gsap.timeline() // gsap timeline simplifies sequences for animations
-//       t1.from("#intro-slider", {
-//         //  yPercent: "-100", 
-//          duration: 0.1,
-//          delay:0.1,
-//          paused:false,
-//       }).from(["#title-1", "#title-2", "#title-3", "#title-4", "#title-5"], {
-//         opacity: 0,
-//         y: "+=30",
-//         stagger:0.1,
-//       }).to(["#title-1", "#title-2", "#title-3", "#title-4", "#title-5"], {
-//         opacity: 0,
-//         y: "-=30",
-//         delay:0.1,
-//         stagger: 0.1,
-//       }).to("#intro-slider", {
-//         yPercent: "-100",
-//         duration: 0.1,
-//       }).from("#app", {
-//         opacity:1,
-//         duration: 0.1,
-//       })
-//     }, comp)
-//     return () => ctx.revert() // prevents memory leak with the library
-//   }, []) 
-
-
-//   return (
-//     <div className="app-container" ref={comp}>
-//       <div 
-//         id="intro-slider"
-//         className="Intro"
-//       >
-//         <div className="role" id="title-1">Goal-Oriented</div>
-//         <div className="role" id="title-2">Problem-Solver</div>
-//         <div className="role" id="title-3">Determined</div>
-//         <div className="role" id="title-4">CS Student at the University of Houston</div>
-//         <div className="role" id="title-5">Jackson Nace</div>
-//       </div>
-//       <div className="App" id="app">
-//         <Navbar />
-//         <Hero/>
-//         {/* <About/> */}
-//         <Experience/>
-//         <Education />
-//         <Projects />
-//         <Certs />
-//         <Footer />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About"
+import Projects from "./pages/Projects"
+import Experience from "./pages/Experience"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar";
 
@@ -96,7 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<Projects />} />
         </Routes>
         <Footer />
       </div>
---
+++ portfolio/src/pages/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TopProjects from "../components/TopProjects"
+
+function Projects() {
+  return (
+    <div>
+      <TopProjects/>
+    </div>
+  )
+}
+
+export default Projects
