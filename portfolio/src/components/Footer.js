0d27a997545e5d7c021e910d00109a32d5032c94
JacksonNace <jacksonnace964@gmail.com>
2025-02-04 15:35:03 -0600

    align about section, footer

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,17 +1,21 @@
 import React from "react";
 import "./Footer.css";
+import { IoIosLink } from "react-icons/io";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-message">Thanks for visiting!</div>
-      <div className="footer-content">
+      <div className="footer-left">
+        <div className="footer-message">Thanks for visiting!</div>
+        <div className="footer-copyright">&copy; 2025 Jackson Nace</div>
+      </div>
+      <div className="footer-right">
         <div className="footer-socials">
           <a href="https://github.com/JacksonNace" target="_blank" rel="noopener noreferrer">
-            Github
+            Github <IoIosLink />
           </a>
           <a href="https://www.linkedin.com/in/jacksonnace/" target="_blank" rel="noopener noreferrer">
-            LinkedIn
+            LinkedIn <IoIosLink />
           </a>
         </div>
       </div>
