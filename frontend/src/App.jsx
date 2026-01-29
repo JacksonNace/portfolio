238d70566270b921f1dc2d400fcf1d5762fa0557
Alyssa To <142935267+alyssaxto@users.noreply.github.com>
2026-01-28 22:28:13 -0600

    custom cursor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import CustomCursor from "./CustomCursor";
 
 function App() {
   return (
     <div className="app-container">
+            <CustomCursor />
       <Navbar />
       <Home />
       <Footer />
